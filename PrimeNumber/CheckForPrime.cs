145d2535f586a5527475c4420495287bfb6bad60
Sean Finlayson <seanfin@gmail.com>
2020-05-14 17:12:51 -0700

    Add project files.

---
+++ PrimeNumber/CheckForPrime.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    
+
+    public class CheckForPrime
+    {
+        /// <summary>
+        /// The lower limit of the prime number list.
+        /// </summary>
+        private  int LowerLimit { get; set; }
+
+        /// <summary>
+        /// The upper limit of the prime number list.
+        /// </summary>
+        private  int UpperLimit { get; set; }
+        
+
+        /// <summary>
+        /// Constructor for the Check for Prime Class. 
+        /// </summary>
+        /// <param name="lowerLimit">Lower Limit of the check for prime class. </param>
+        /// <param name="upperLimit">Upper Limit of the check for prime class.</param>
+        public CheckForPrime(int lowerLimit, int upperLimit)
+        {
+            this.LowerLimit = lowerLimit;
+            this.UpperLimit = upperLimit;
+
+        }
+
+
+        /// <summary>
+        /// Retrieves a list of the prime numbers and writes them to the console. 
+        /// </summary>
+        public string ListofPrimeNumbers()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int ctr;
+            for (int num = LowerLimit; num <= UpperLimit; num++)
+            {
+                ctr = 0;
+
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+
+                if (ctr == 0 && num != 1)
+                    sb.Append(String.Format("{0}, ", num));
+
+            }
+
+            string primes =  sb.ToString();
+            primes= primes.Trim().TrimEnd(',');
+            return primes;
+           
+        }
+
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Say hello to the user
+                Console.WriteLine("Welcome to Prime Number!");
+                Console.WriteLine("The purpose of this program is to provide a list of prime numbers based on a lower and upper limit.");
+
+
+                //Get the lower limit.
+                Console.WriteLine("Please specify your lower limit.");
+                string lowerLimitSTR = Console.ReadLine();
+                int lowerLimit = 0;
+                if (!Int32.TryParse(lowerLimitSTR.Trim(), out lowerLimit))
+                {
+
+                    Console.WriteLine("Lower limit must be numeric defaulting to 1.");
+                    lowerLimit = 1;
+                }
+
+                //Get the upper limit.
+                Console.WriteLine("Please specify your upper limit.");
+                string upperLimitSTR = Console.ReadLine();
+                int upperLimit = 0;
+                if (!Int32.TryParse(upperLimitSTR.Trim(), out upperLimit))
+                {
+                    Console.WriteLine("Upper limit must be numeric defaulting to 20.");
+                    upperLimit = 20;
+                }
+
+                //Instiate the class. 
+                CheckForPrime prime = new CheckForPrime(lowerLimit, upperLimit);
+
+                //Get List of Prime numbers 
+                string primes = prime.ListofPrimeNumbers();
+
+                //Wriite out to console
+                Console.WriteLine("The prime numbers are:");
+                Console.WriteLine(primes);
+
+                //Read to line.
+                Console.ReadLine();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(String.Format("There was an exception while attemption to find the prime numbers.", ex));
+            }
+        }
+    }
+}
